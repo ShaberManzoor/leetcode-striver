5a3d9a65623d7d2a6ca3ebd11ba5156cb2ee69e3
Shaber Manzoor <shabermanzoor@Mac.lan>
2026-05-17 15:26:24 +0530

    Search in rotated sorted array

---
+++ binarySearch/OneDArray/RotatedArraySearch.java
@@ -0,0 +1,31 @@
+package binarySearch.OneDArray;
+
+public class RotatedArraySearch {
+    public static void main(String[] args) {
+        int arr[] = {4,5,6,7,0,1,2};
+        System.out.println(search(arr, 1));
+    }
+    public static int search(int[] nums, int target) {
+        int low = 0, high = nums.length-1, ans = -1;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                break;
+            }else if(nums[mid]>=nums[low]){
+                if(nums[mid]>target && nums[low]<=target){
+                    high = mid-1;
+                }else {
+                    low = mid+1;
+                }
+            }else{
+                if(nums[mid]<target && nums[high]>=target){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        return ans;
+    }
+}
