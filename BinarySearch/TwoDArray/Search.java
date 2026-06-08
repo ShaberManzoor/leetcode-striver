edb1e4687d4ebaf199ef57c65bb54c51528c9d1d
Shaber Manzoor <kshaber313@gmail.com>
2026-06-08 13:41:34 +0530

    Search in a 2D array

---
+++ BinarySearch/TwoDArray/Search.java
@@ -0,0 +1,34 @@
+package BinarySearch.TwoDArray;
+
+public class Search {
+    public static void main(String[] args) {
+        int mat[][] = {
+                { 1, 3, 5, 7 },
+                { 10, 11, 16, 20 },
+                { 23, 30, 34, 60 }
+        };
+
+        System.out.println(searchMatrix(mat, 23));
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}
