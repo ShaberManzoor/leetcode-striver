6e531c59db5a512f6c0ff95272214ed72b11dbb1
Shaber Manzoor <kshaber313@gmail.com>
2026-06-15 12:22:42 +0530

    Linked List Cycle II

---
+++ LinkedList/MediumLL/DetectLoop.java
@@ -0,0 +1,36 @@
+package LinkedList.MediumLL;
+
+public class DetectLoop {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                //start slow from head again
+                slow = head;
+                while(slow!=fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}
