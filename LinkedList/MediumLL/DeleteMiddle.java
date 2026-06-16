ba87989abac8ad75413581d43b35001b5fd81294
Shaber Manzoor <shabermanzoor@Mac.lan>
2026-06-16 22:24:07 +0530

    Delete middle node

---
+++ LinkedList/MediumLL/DeleteMiddle.java
@@ -0,0 +1,17 @@
+package LinkedList.MediumLL;
+
+public class DeleteMiddle {
+    public ListNode deleteMiddle(ListNode head){
+        if(head.next==null) return null;
+
+        ListNode slow = head, fast = head, prev = null;
+        while(fast!=null && fast.next!=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = slow.next;
+        return head;
+    }
+}
