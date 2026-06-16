64d4a1b4959a362a6782347e197d082bc15baab4
Shaber Manzoor <shabermanzoor@Mac.lan>
2026-06-16 21:53:02 +0530

    Rotated Array Search II

---
+++ BinarySearch/OneDArray/RotateArraySearch2.java
@@ -0,0 +1,35 @@
+package binarySearch.OneDArray;
+
+public class RotateArraySearch2 {
+    public static void main(String[] args) {
+        int arr[] = {2,5,6,0,0,1,2};
+        System.out.println(search(arr, 8));
+    }
+    static boolean search(int nums[], int target){
+        int low = 0, high = nums.length-1;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[low]==nums[mid] && nums[high]==nums[mid]){
+                low++;
+                high--;
+            }else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<nums[mid]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(nums[mid]<target && target<=nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
