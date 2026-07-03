80acf22ba1e08b9fafa2a94df1d2d3cc4a972e34
Shaber Manzoor <kshaber313@gmail.com>
2026-07-03 16:38:39 +0530

    Stock Spanner

---
+++ StacksAndQueues/ImpProblems/StockSpanner.java
@@ -0,0 +1,21 @@
+package StacksAndQueues.ImpProblems;
+
+import java.util.Stack;
+
+public class StockSpanner {
+    Stack<int[]> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+        while (!st.isEmpty() && price >= st.peek()[0]) { // If the current price is greater than stack peek.
+            span += st.peek()[1];
+            st.pop();
+        }
+        st.push(new int[] { price, span });
+        return span;
+    }
+}
