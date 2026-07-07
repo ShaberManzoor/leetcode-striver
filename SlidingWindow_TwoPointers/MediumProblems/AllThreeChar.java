f65bc519ecbe81690110bce9b4c45fbc043b0dc2
Shaber Manzoor <kshaber313@gmail.com>
2026-07-07 10:28:19 +0530

    Substrings with all three char

--- SlidingWindow_TwoPointers/MediumProblems/AllThreeChar.java
+++ SlidingWindow_TwoPointers/MediumProblems/AllThreeChar.java
@@ -6,13 +6,21 @@ public class AllThreeChar {
         System.out.println(numberOfSubstrings(s));
     }
     public static int numberOfSubstrings(String s) {
-        int lastSeen[] = {-1, -1, -1};
+        int[] freq = new int[3];
+
+        int left = 0;
         int count = 0;
-        for(int i=0;i<s.length();i++){
-            lastSeen[s.charAt(i)-'a'] = i;
-            
-            count += 1 + Math.min(Math.min(lastSeen[0], lastSeen[1]), lastSeen[2]);
-            
+
+        for (int right = 0; right < s.length(); right++) {
+
+            freq[s.charAt(right) - 'a']++;
+
+            while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
+                freq[s.charAt(left) - 'a']--;
+                left++;
+            }
+
+            count += left;
         }
 
         return count;
