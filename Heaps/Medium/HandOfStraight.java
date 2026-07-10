90c07ce7f3f8a9bbe650328345fa26fd0cdd1300
Shaber Manzoor <shabermanzoor@Mac.lan>
2026-07-10 22:41:53 +0530

    Hand of Straight

--- Heaps/Medium/HandOfStraight.java
+++ Heaps/Medium/HandOfStraight.java
@@ -3,7 +3,13 @@ package Heaps.Medium;
 import java.util.TreeMap;
 
 public class HandOfStraight {
-    public boolean isNStraightHand(int[] hand, int groupSize) {
+    public static void main(String[] args) {
+        int hand[] = {1,2,3,6,2,3,4,7,8};
+        int groupSize = 3;
+
+        System.out.println(isNStraightHand(hand, groupSize));
+    }
+    public static boolean isNStraightHand(int[] hand, int groupSize) {
         if(hand.length % groupSize != 0) return false;
 
         TreeMap<Integer, Integer> freq = new TreeMap<>(); //to store the frequency of each card value
