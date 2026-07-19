f567b3ee5191223f07c3d542490f9d646051f7e8
Shaber Manzoor <shabermanzoor@Mac.lan>
2026-07-19 12:38:59 +0530

    Max Depth in BT

---
+++ BinaryTrees/Medium/MaxDepth.java
@@ -0,0 +1,14 @@
+package BinaryTrees.Medium;
+
+import BinaryTrees.TreeNode;
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+
+        return Math.max(lh, rh) + 1;
+    }
+}
