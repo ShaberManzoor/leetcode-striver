0741a62619781dc542da40ef999bf74fd64e3f7c
Shaber Manzoor <shabermanzoor@Mac.lan>
2026-07-25 18:24:37 +0530

    Insert in BST

---
+++ BinarySearchTree/Insert.java
@@ -0,0 +1,28 @@
+package BinarySearchTree;
+
+public class Insert {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode node = new TreeNode(val);
+
+        if(root==null) return node;
+
+        TreeNode curr = root;
+        while(curr!=null) {
+            if(curr.val>val) {
+                if(curr.left==null) {
+                    curr.left = node;
+                    break;
+                }
+                curr = curr.left;
+            } else {
+                if(curr.right==null) {
+                    curr.right = node;
+                    break;
+                }
+                curr = curr.right;
+            }
+        }
+
+        return root;
+    }
+}
